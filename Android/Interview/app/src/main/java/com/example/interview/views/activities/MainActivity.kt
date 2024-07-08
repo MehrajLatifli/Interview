38357ef7164663e4ebed8fd4844c67925e77ab86
Mehrac Lətifli <meracletifli@gmail.com>
2024-07-09 03:30:28 +0400

    GOD bless universes

--- Android/Interview/app/src/main/java/com/example/interview/views/activities/MainActivity.kt
+++ Android/Interview/app/src/main/java/com/example/interview/views/activities/MainActivity.kt
@@ -6,16 +6,22 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.example.interview.R
+import com.example.interview.databinding.ActivityMainBinding
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
+
+    lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
+
+
+
     }
 }
